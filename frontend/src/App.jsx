038b5dc9e0133b728189ee5e4644523d90172f2c
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-06 13:33:03 +0530

    Routes protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,98 +1,93 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import "./index.css"
-import "./custom.css"
-import Login from "./components/shared/Login"
-import SignUp from "./components/shared/SignUp"
-import Home from "./components/User/Home"
-import Jobs from "./components/User/Jobs"
-import Browse from "./components/User/Browse"
-import Profile from "./components/User/Profile"
-import JobDescription from "./components/shared/JobDescription"
-import UpdateProfile from "./components/shared/UpdateProfile"
-import Companies from "./components/admin/Companies"
-import CreateCompany from "./components/admin/CreateCompany"
-import CompanyPage from "./components/admin/CompanyPage"
-import EditCompany from "./components/admin/EditCompany"
-import CreateJob from "./components/admin/CreateJob"
-import AdminJobs from "./components/admin/AdminJobs"
-import JobPage from "./components/admin/JobPage"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import "./custom.css";
+import Login from "./components/shared/Login";
+import SignUp from "./components/shared/SignUp";
+import Home from "./components/User/Home";
+import Jobs from "./components/User/Jobs";
+import Browse from "./components/User/Browse";
+import Profile from "./components/User/Profile";
+import JobDescription from "./components/shared/JobDescription";
+import UpdateProfile from "./components/shared/UpdateProfile";
+import Companies from "./components/admin/Companies";
+import CreateCompany from "./components/admin/CreateCompany";
+import CompanyPage from "./components/admin/CompanyPage";
+import EditCompany from "./components/admin/EditCompany";
+import CreateJob from "./components/admin/CreateJob";
+import AdminJobs from "./components/admin/AdminJobs";
+import JobPage from "./components/admin/JobPage";
+import ProtectedRoute from "./components/protectedRoute";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <Home />,
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "/signup",
-    element: <SignUp />
+    element: <SignUp />,
   },
   {
     path: "/jobs",
-    element: <Jobs />
+    element: <Jobs />,
   },
   {
     path: "/explore",
-    element: <Browse />
+    element: <Browse />,
   },
   {
     path: "/profile",
-    element: <Profile />
+    element: <ProtectedRoute element={<Profile />} />,
   },
   {
     path: "/profile/edit",
-    element: <UpdateProfile />
+    element: <ProtectedRoute element={<UpdateProfile />} />,
   },
   {
     path: "/description/:id",
-    element: <JobDescription />
+    element: <ProtectedRoute element={<JobDescription />} />,
   },
   {
     path: "/admin/companies",
-    element: <Companies />
+    element: <ProtectedRoute element={<Companies />} />,
   },
   {
     path: "/admin/jobs",
-    element: <AdminJobs />
+    element: <ProtectedRoute element={<AdminJobs />} />,
   },
   {
     path: "/admin/companies/create",
-    element: <CreateCompany />
+    element: <ProtectedRoute element={<CreateCompany />} />,
   },
   {
     path: "/admin/companies/:id",
-    element: <CompanyPage />
+    element: <ProtectedRoute element={<CompanyPage />} />,
   },
   {
     path: "/admin/companies/:id/edit",
-    element: <EditCompany />
+    element: <ProtectedRoute element={<EditCompany />} />,
   },
   {
     path: "/admin/company/:id/jobs/create",
-    element: <CreateJob />
+    element: <ProtectedRoute element={<CreateJob />} />,
   },
   {
     path: "/admin/jobpage/:id",
-    element: <JobPage />
+    element: <ProtectedRoute element={<JobPage />} />,
   },
-  
-])
+]);
 
 function App() {
-
-  return (
-    <>
-      <RouterProvider router={appRouter}/>
-                
-    </>
-  )
+  return <RouterProvider router={appRouter} />;
 }
 
-export default App
+export default App;
+
+
 
         
   
--- frontend/src/components/protectedRoute.jsx
+++ frontend/src/components/protectedRoute.jsx
@@ -1,5 +1,6 @@
 import { isAuthenticated } from '@/lib/utils';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const ProtectedRoute = ({ element }) => {
   const isUserAuthenticated = isAuthenticated();
@@ -9,6 +10,9 @@ const ProtectedRoute = ({ element }) => {
   }
 
   return element;
+};
+ProtectedRoute.propTypes = {
+  element: PropTypes.element.isRequired,
 };
 
 export default ProtectedRoute;

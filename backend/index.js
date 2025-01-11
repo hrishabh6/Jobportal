7ed75e3715cb5e4230146f64af348f53f7916c1e
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-11 12:59:00 +0530

    Vercel.json created

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow the deployed frontend origin 
-    credentials: true, // Allow credentials (cookies, authorization headers)
-}));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000
 app.use('/api/v1/user', userRoute)

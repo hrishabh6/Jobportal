ab15ed5fedef64a39f89a9864c703c4dbb51e9f2
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-11 12:53:59 +0530

    Ready for dep

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,18 @@ import companyRoute from './routes/company.route.js'
 import jobRoute from './routes/job.route.js'
 import applicationsRoute from './routes/application.route.js'
 import globalsearchRoute from './routes/globalsearch.route.js'
-import path from 'path';
 dotenv.config({})
 
 const app = express()
-
-const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow the deployed frontend origin 
+    credentials: true, // Allow credentials (cookies, authorization headers)
+}));
 
 const PORT = process.env.PORT || 3000
 app.use('/api/v1/user', userRoute)
@@ -27,10 +28,6 @@ app.use('/api/v1/jobs', jobRoute)
 app.use('/api/v1/application', applicationsRoute)
 app.use('/api/v1/globalsearch', globalsearchRoute)
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-})
 app.listen(PORT, () => {
     connectToDatabase()
     console.log(`Server is running on port ${PORT}`)
